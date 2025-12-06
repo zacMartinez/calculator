d50b6457918206bdafc33f1bbcec70ba51dc22b9
Zachary Martinez <zach.martinez@protonmail.com>
2025-12-05 16:27:30 -0800

    Add 'operate' function and prevent divide by 0 I created the 'operate' function. I also decided to check for an invalid divide by zero operation at this level. I added a global variable to store a silly error message string.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const funnyError = '😠Get out of my swamp!😠'
+let operandX;
+let operandY;
+let operator;
+
 function add(x, y) {
     return x + y;
 }
@@ -12,4 +17,20 @@ function multiply(x, y) {
 
 function divide(x, y) {
     return x / y;
+}
+
+function operate(x, y, operator) {
+    switch (operator) {
+        case '*':
+            return multiply(x, y);
+        case '/':
+            if (y === 0) return funnyError;
+            return divide(x, y);
+        case '+':
+            return add(x, y);            
+        case '-':
+            return subtract(x, y);
+        default:
+            return 'Invalid operation';
+    }
 }
