671de162b700531e2bdacd1f1bb2fed3190cae4a
Zachary Martinez <zach.martinez@protonmail.com>
2025-12-19 13:18:25 -0800

    Redo operation functions and global variables

---
+++ script.js
@@ -0,0 +1,21 @@
+const MAX_DECIMAL = 4;
+
+let X = 0;
+let Y = 0;
+let operator = null;
+
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
