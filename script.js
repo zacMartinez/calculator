39eb84d4929a9042bc75e94a637f5a9c1d92ba33
Zachary Martinez <zach.martinez@protonmail.com>
2026-02-13 08:35:28 -0800

    Achieve basic functionality.

--- script.js
+++ script.js
@@ -5,14 +5,15 @@ const buttons = document.querySelector('#buttons');
 const inputDisplay = document.querySelector('#inputDisplay');
 
 const operations = {
-    '+': add,
-    '-': subtract,
     '*': multiply,
+    '+': add,
     '/': divide,
+    '-': subtract, // subtraction needs to be at the end to avoid regex problem
 };
+const operators = new RegExp(`[${Object.keys(operations).join('')}]`);
 
-let X = 0;
-let Y = 0;
+let X = null;
+let Y = null;
 let operator = null;
 
 function add(x, y) {
@@ -35,37 +36,71 @@ function operate(x, y, operator) {
     return +(operations[operator](x, y)).toFixed(MAX_DECIMAL); // +() ensures value is a number
 }
 
-function evaluateBuffer() {
-   console.log(`X:${X}, Y:${Y}, operator:${operator}`);
+function reset() {
+    X = null;
+    Y = null;
+    operator = null;
 }
 
-function handleNonNumeric(input) {
-    const operators = Object.keys(operations);
+function evaluateExpression (x, y, operator) {
+    const answer = operate(x,y, operator);
+    reset();
+    X = answer;
+    BUFFER.splice(0);
+    BUFFER.push(answer);
+}
 
-    if (operators.includes(input)) {
-        if (BUFFER.length > 0) {
-            operator ||= input;
-        }
+function handleDecimal(input) {
+    console.log('I am a decimal.');
+}
+
+function handleOperators(input) {
+    if (operator) {
+        evaluateExpression(X, Y, operator);
     }
+    operator = input;
+    BUFFER.push(input);
+    console.log('I am an operator.');
+}
+
+function handleEquals() {
+    if (X && Y && operator) {
+        evaluateExpression(X, Y, operator);
+    }
+}
+
+function handleDigits(input) {
+    BUFFER.push(input);
+    const numbers = BUFFER.join('').split(operators);
+    X = parseInt(numbers[0]);
+    Y = parseInt(numbers[1]);
 }
 
 buttons.addEventListener('click', (e) => {
-    const inputValue = e.target.value;
-
+    const input = e.target.value;
+    
     switch (true) {
-        case inputValue === 'clear' :
-            BUFFER.splice(0, BUFFER.length);
+        case input === 'clear' :
+            BUFFER.splice(0);
+            reset();
             break;
-        case inputValue === 'backspace' :
+        case input === 'backspace' :
             BUFFER.pop(1);
             break;
-        case /[0-9]/.test(inputValue):
-            BUFFER.push(inputValue);
+        case input === '.' :
+            handleDecimal(input);
+            break;
+        case input === '=' :
+            handleEquals();
+            break;
+        case operators.test(input) :
+            handleOperators(input)
             break;
         default :
-            handleNonNumeric(inputValue);
+            handleDigits(input);
+            break;      
     }
     
-    evaluateBuffer();
     inputDisplay.value = BUFFER.join('');
+    console.log(`X: ${X}, Y: ${Y}, operator: ${operator}`);
 });
